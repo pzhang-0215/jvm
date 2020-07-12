0bd7da98b08e890220cb704a0f2f58f13b0e9a4d
pzhang <1285916518@qq.com>
2020-07-12 13:13:32 +0800

    update code

--- src/com/zp/loadclass/LoadClassDemo.java
+++ src/com/zp/loadclass/LoadClassDemo.java
@@ -3,15 +3,15 @@ package com.zp.loadclass;
 /**
  * <h3>jvm</h3>
  * <p>类加载器的使用</p>
- *  类加载器的使用
+ * 对于静态字段来说，只有直接定义了该字段的类才会被初始化；eg:System.out.println(MyChild1.str);
+ *  当一个类初始化时，要求其父类全部都已经被初始化了；eg：System.out.println(MyChild1.str2);
  * @author : zp
  * @date : 2020-06-29 12:42
  **/
 public class LoadClassDemo {
     public static void main(String[] args) {
-//        System.out.println(MyChild1.str);
-
-        System.out.println(MyChild1.str2);
+        System.out.println(MyChild1.str);
+//        System.out.println(MyChild1.str2);
     }
 }
 
